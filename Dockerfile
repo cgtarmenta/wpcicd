4c6786ea61a0493c5f711296b094a275af5f0e6a
Tadeo Armenta <cgtarmenta@gmail.com>
2022-12-10 20:56:56 +0100

    first working atempts

--- Dockerfile
+++ Dockerfile
@@ -1,72 +1,119 @@
-FROM ubuntu/nginx:1.18-22.04_edge
+FROM php:8.1-fpm-alpine
 
 LABEL MANTAINER="Tadeo Armenta <contact@tadeoarmenta.com>"
 
-# Let the container know that there is no tty
-ENV DEBIAN_FRONTEND noninteractive
-# At this point is the only one supported by wordpress echosystem
-ENV PHP_VERSION 7.4
+# persistent dependencies
+RUN set -eux; \
+	apk add --no-cache \
+# in theory, docker-entrypoint.sh is POSIX-compliant, but priority is a working, consistent image
+		bash \
+# Ghostscript is required for rendering PDF previews
+		ghostscript \
+# Alpine package for "imagemagick" contains ~120 .so files, see: https://github.com/docker-library/wordpress/pull/497
+		imagemagick \
+	;
 
-# Add php repo
-RUN set -x \
-    && apt-get update \
-    && apt -y install software-properties-common dirmngr apt-transport-https lsb-release ca-certificates \
-    && add-apt-repository ppa:ondrej/php
-# Install Craft Requirements
-RUN set -x \
-    && apt-get update \
-    && apt-get install -yq --no-install-recommends \
-        apt-utils \
-        curl \
-        iproute2 \
-        software-properties-common \
-        unzip \
-        zip \
-    && apt-get update && apt-get install -yq --no-install-recommends \
-        php${PHP_VERSION}-cli \
-        php${PHP_VERSION}-curl \
-        php${PHP_VERSION}-fpm \
-        php${PHP_VERSION}-mysql \
-        php${PHP_VERSION}-gd \
-        php${PHP_VERSION}-gmp \
-        php${PHP_VERSION}-intl \
-        php${PHP_VERSION}-json \
-        php${PHP_VERSION}-mbstring \
-        php${PHP_VERSION}-opcache \
-        php${PHP_VERSION}-readline \
-        php${PHP_VERSION}-soap \
-        php${PHP_VERSION}-xml \
-        php${PHP_VERSION}-zip \
-        php${PHP_VERSION}-imagick \
-        php${PHP_VERSION}-redis \
-    && echo "#!/bin/sh\nexit 0" > /usr/sbin/policy-rc.d \
-    && apt-get autoremove --purge -y \
-        software-properties-common \
-    && apt-get clean autoclean \
-    && apt-get autoremove --yes \
-    && rm -rf /var/lib/{apt,dpkg,cache,log}/
+# install the PHP extensions we need (https://make.wordpress.org/hosting/handbook/handbook/server-environment/#php-extensions)
+RUN set -ex; \
+	\
+	apk add --no-cache --virtual .build-deps \
+		$PHPIZE_DEPS \
+		freetype-dev \
+		icu-dev \
+		imagemagick-dev \
+		libjpeg-turbo-dev \
+		libpng-dev \
+		libwebp-dev \
+		libzip-dev \
+	; \
+	\
+	docker-php-ext-configure gd \
+		--with-freetype \
+		--with-jpeg \
+		--with-webp \
+	; \
+	docker-php-ext-install -j "$(nproc)" \
+		bcmath \
+		exif \
+		gd \
+		intl \
+		mysqli \
+		zip \
+	; \
+# WARNING: imagick is likely not supported on Alpine: https://github.com/Imagick/imagick/issues/328
+# https://pecl.php.net/package/imagick
+	pecl install imagick-3.6.0; \
+	docker-php-ext-enable imagick; \
+	rm -r /tmp/pear; \
+	\
+# some misbehaving extensions end up outputting to stdout 🙈 (https://github.com/docker-library/wordpress/issues/669#issuecomment-993945967)
+	out="$(php -r 'exit(0);')"; \
+	[ -z "$out" ]; \
+	err="$(php -r 'exit(0);' 3>&1 1>&2 2>&3)"; \
+	[ -z "$err" ]; \
+	\
+	extDir="$(php -r 'echo ini_get("extension_dir");')"; \
+	[ -d "$extDir" ]; \
+	runDeps="$( \
+		scanelf --needed --nobanner --format '%n#p' --recursive "$extDir" \
+			| tr ',' '\n' \
+			| sort -u \
+			| awk 'system("[ -e /usr/local/lib/" $1 " ]") == 0 { next } { print "so:" $1 }' \
+	)"; \
+	apk add --no-network --virtual .wordpress-phpexts-rundeps $runDeps; \
+	apk del --no-network .build-deps; \
+	\
+	! { ldd "$extDir"/*.so | grep 'not found'; }; \
+# check for output like "PHP Warning:  PHP Startup: Unable to load dynamic library 'foo' (tried: ...)
+	err="$(php --version 3>&1 1>&2 2>&3)"; \
+	[ -z "$err" ]
+# Install nginx
+RUN apk add --no-cache nginx
+# set recommended PHP.ini settings
+# see https://secure.php.net/manual/en/opcache.installation.php
+RUN set -eux; \
+	docker-php-ext-enable opcache; \
+	{ \
+		echo 'opcache.memory_consumption=128'; \
+		echo 'opcache.interned_strings_buffer=8'; \
+		echo 'opcache.max_accelerated_files=4000'; \
+		echo 'opcache.revalidate_freq=2'; \
+		echo 'opcache.fast_shutdown=1'; \
+	} > /usr/local/etc/php/conf.d/opcache-recommended.ini
+# https://wordpress.org/support/article/editing-wp-config-php/#configure-error-logging
+RUN { \
+# https://www.php.net/manual/en/errorfunc.constants.php
+# https://github.com/docker-library/wordpress/issues/420#issuecomment-517839670
+		echo 'error_reporting = E_ERROR | E_WARNING | E_PARSE | E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_COMPILE_WARNING | E_RECOVERABLE_ERROR'; \
+		echo 'display_errors = Off'; \
+		echo 'display_startup_errors = Off'; \
+		echo 'log_errors = On'; \
+		echo 'error_log = /dev/stderr'; \
+		echo 'log_errors_max_len = 1024'; \
+		echo 'ignore_repeated_errors = On'; \
+		echo 'ignore_repeated_source = Off'; \
+		echo 'html_errors = Off'; \
+	} > /usr/local/etc/php/conf.d/error-logging.ini
 
-# make sure run/php exists and is read/write accesible 
+# # PHP config
 RUN mkdir -p /run/php && chown www-data:www-data /run/php
-# make sure there is a log folder and is read/write accesible 
+COPY php/www.conf /usr/local/etc/php-fpm.d/www.conf
+
+# # make sure there is a log folder and is read/write accesible 
 RUN mkdir -p /usr/share/nginx/logs && chown www-data:www-data /usr/share/nginx/logs
 RUN mkdir -p /usr/share/nginx/html/shared/nginx_logs && chown www-data:www-data /usr/share/nginx/html/shared/nginx_logs
-# make sure there is a user folder and is read/write accesible
+# # make sure there is a user folder and is read/write accesible
 RUN mkdir -p /home/ubuntu && chown -R www-data:www-data /home/ubuntu
-# remove default files
-RUN rm -rf /usr/share/nginx/html/* && rm -rf /etc/nginx/sites-available/*
-# PHP config
-COPY php /etc/php
-# Entrypoints
-COPY scripts/php.sh /docker-entrypoint.d/
-# Nginx config
-COPY nginx/nginx.conf /etc/nginx/nginx.conf
-COPY nginx/default.conf /etc/nginx/sites-available/default
-# Update nginx to match worker_processes to # of cpu's
+
+# # Nginx config
+# RUN mkdir -p /etc/nginx/{'modules-enabled','conf.d','sites-enabled'}
+# COPY nginx/nginx.conf /etc/nginx/nginx.conf
+COPY nginx/default.conf /etc/nginx/http.d/default.conf
+# # Update nginx to match worker_processes to # of cpu's
 RUN procs=$(cat /proc/cpuinfo |grep processor | wc -l); sed -i -e "s/worker_processes  1/worker_processes $procs/" /etc/nginx/nginx.conf
-# Set the actual content
+# # Set the actual content
 COPY ./site/wordpress /usr/share/nginx/html/release
-# Clean wp bloatware
+# # Clean wp bloatware
 RUN rm -rf /usr/share/nginx/html/release/wp-content
 
 # Link external ones
@@ -75,8 +122,15 @@ RUN cd /usr/share/nginx/html/release && ln -s ../shared/wp-config.php wp-config.
 
 # Always chown webroot for better mounting
 RUN chown -Rf www-data.www-data /usr/share/nginx
-
-# Exports
+# work entrypoints
+COPY scripts/entrypoint.sh /docker-entrypoint.sh
+RUN mkdir -p /docker-entrypoint.d
+COPY scripts/30-tune-worker-processes.sh /docker-entrypoint.d
+# # Exports
 EXPOSE 80
 VOLUME /usr/share/nginx/html/shared
 WORKDIR /usr/share/nginx/html/release
+
+ENTRYPOINT ["/docker-entrypoint.sh"]
+
+CMD ["/bin/bash", "-c", "php-fpm & nginx -g 'daemon off;'"]
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,251 +1,63 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -Eeuo pipefail
 
-set -euo pipefail
+if [ "$1" = 'php-fpm' ]; then
+	uid="$(id -u)"
+	gid="$(id -g)"
+	if [ "$uid" = '0' ]; then
+		user='www-data'
+		group='www-data'
+	else
+		user="$uid"
+		group="$gid"
+	fi
 
-MOUNTPOINT=/usr/share/nginx/html
+	if [ ! -e index.php ] && [ ! -e wp-includes/version.php ]; then
+		# if the directory exists and WordPress doesn't appear to be installed AND the permissions of it are root:root, let's chown it (likely a Docker-created directory)
+		if [ "$uid" = '0' ] && [ "$(stat -c '%u:%g' .)" = '0:0' ]; then
+			chown "$user:$group" .
+		fi
+	fi
 
-# for dir in shared release; do
-#     if [[ ! -d $MOUNTPOINT/$dir && -d /wordpress/$dir ]]; then
-#         ln -s /wordpress/$dir $MOUNTPOINT/$dir && echo "Linked $MOUNTPOINT/$dir to /wordpress/$dir."
-#     fi
-# done
-
-file_env() {
-  local var="$1"
-  local fileVar="${var}_FILE"
-  local def="${2:-}"
-  if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
-    echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
-    exit 1
+fi
+entrypoint_log() {
+  if [ -z "${NGINX_ENTRYPOINT_QUIET_LOGS:-}" ]; then
+    echo "$@"
   fi
-  local val="$def"
-  if [ "${!var:-}" ]; then
-    val="${!var}"
-  elif [ "${!fileVar:-}" ]; then
-    val="$(< "${!fileVar}")"
-  fi
-  export "$var"="$val"
-  unset "$fileVar"
 }
 
-[ -d $MOUNTPOINT/release ] || mkdir $MOUNTPOINT/release
+if [ "$1" = "nginx" -o "$1" = "nginx-debug" ]; then
+  if /usr/bin/find "/docker-entrypoint.d/" -mindepth 1 -maxdepth 1 -type f -print -quit 2>/dev/null | read v; then
+      entrypoint_log "$0: /docker-entrypoint.d/ is not empty, will attempt to perform configuration"
 
-cd $MOUNTPOINT/release
+      entrypoint_log "$0: Looking for shell scripts in /docker-entrypoint.d/"
+      find "/docker-entrypoint.d/" -follow -type f -print | sort -V | while read -r f; do
+          case "$f" in
+              *.envsh)
+                  if [ -x "$f" ]; then
+                      entrypoint_log "$0: Sourcing $f";
+                      . "$f"
+                  else
+                      # warn on shell scripts without exec bit
+                      entrypoint_log "$0: Ignoring $f, not executable";
+                  fi
+                  ;;
+              *.sh)
+                  if [ -x "$f" ]; then
+                      entrypoint_log "$0: Launching $f";
+                      "$f"
+                  else
+                      # warn on shell scripts without exec bit
+                      entrypoint_log "$0: Ignoring $f, not executable";
+                  fi
+                  ;;
+              *) entrypoint_log "$0: Ignoring $f";;
+          esac
+      done
 
-  # allow any of these "Authentication Unique Keys and Salts" to be specified
-  # via environment variables with a `WORDPRESS_` prefix
-  # (e.g. `WORDPRESS_AUTH_KEY` for `AUTH_KEY`)
-  uniqueEnvs=(
-    AUTH_KEY
-    SECURE_AUTH_KEY
-    LOGGED_IN_KEY
-    NONCE_KEY
-    AUTH_SALT
-    SECURE_AUTH_SALT
-    LOGGED_IN_SALT
-    NONCE_SALT
-  )
-
-  envs=(
-    WORDPRESS_DB_HOST
-    WORDPRESS_DB_USER
-    WORDPRESS_DB_PASSWORD
-    WORDPRESS_DB_NAME
-    "${uniqueEnvs[@]/#/WORDPRESS_}"
-    WORDPRESS_TABLE_PREFIX
-    WORDPRESS_DEBUG
-    WORDPRESS_URL
-  )
-
-  haveConfig=
-
-  for e in "${envs[@]}"; do
-    file_env "$e"
-    if [ -z "$haveConfig" ] && [ -n "${!e}" ]; then
-      haveConfig=1
-    fi
-  done
-
-  # linking backwards-compatibility
-  if [ -n "${!MYSQL_ENV_MYSQL_*}" ]; then
-    haveConfig=1
-    # host defaults to "mysql" below if unspecified
-    : "${WORDPRESS_DB_USER:=${MYSQL_ENV_MYSQL_USER:-root}}"
-    if [ "$WORDPRESS_DB_USER" = 'root' ]; then
-      : "${WORDPRESS_DB_PASSWORD:=${MYSQL_ENV_MYSQL_ROOT_PASSWORD:-}}"
-    else
-      : "${WORDPRESS_DB_PASSWORD:=${MYSQL_ENV_MYSQL_PASSWORD:-}}"
-    fi
-    : "${WORDPRESS_DB_NAME:=${MYSQL_ENV_MYSQL_DATABASE:-}}"
+      entrypoint_log "$0: Configuration complete; ready for start up"
+  else
+      entrypoint_log "$0: No files found in /docker-entrypoint.d/, skipping configuration"
   fi
-
-  # only touch "wp-config.php" if we have environment-supplied configuration values
-  if [ "$haveConfig" ]; then
-    : "${WORDPRESS_DB_HOST:=mysql}"
-    : "${WORDPRESS_DB_USER:=root}"
-    : "${WORDPRESS_DB_PASSWORD:=}"
-    : "${WORDPRESS_DB_NAME:=wordpress}"
-
-    # version 4.4.1 decided to switch to windows line endings, that breaks our seds and awks
-    # https://github.com/docker-library/wordpress/issues/116
-    # https://github.com/WordPress/WordPress/commit/1acedc542fba2482bab88ec70d4bea4b997a92e4
-    sed -ri -e 's/\r$//' wordpress/wp-config*
-
-if [ ! -e $MOUNTPOINT/shared/wp-config.php ]; then
-      awk '/^\/\*.*stop editing.*\*\/$/ && c == 0 { c = 1; system("cat") } { print }' wordpress/wp-config-sample.php > $MOUNTPOINT/shared/wp-config.php << 'EOPHP'
-    // If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
-    // see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
-    if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-    $_SERVER['HTTPS'] = 'on';
-    }
-
-    define('WP_HOME', 'unset');
-    define('WP_SITEURL', 'unset');
-    define('WP_CONTENT_DIR', '/var/www/html/shared/wp-content/');
-    define('WP_CONTENT_URL', 'unset');
-    define('WP_PLUGIN_URL', 'unset');
-    define('FS_METHOD', 'direct');
-
-    # Leave this set to false. We're in a container, so we will
-    # never be able to update from inside of Wordpress
-    define( 'WP_AUTO_UPDATE_CORE', false );
-
-    # Disable file editing
-    define('DISALLOW_FILE_EDIT', true);
-
-EOPHP
-    chown www-data:www-data $MOUNTPOINT/shared/wp-config.php
-    chmod 640 $MOUNTPOINT/shared/wp-config.php
 fi
-
-    # see http://stackoverflow.com/a/2705678/433558
-    sed_escape_lhs() {
-      echo "$@" | sed -e 's/[]\/$*.^|[]/\\&/g'
-    }
-
-    sed_escape_rhs() {
-      echo "$@" | sed -e 's/[\/&]/\\&/g'
-    }
-
-    php_escape() {
-      local escaped="$(php -r 'var_export(('"$2"') $argv[1]);' -- "$1")"
-      if [ "$2" = 'string' ] && [ "${escaped:0:1}" = "'" ]; then
-        escaped="${escaped//$'\n'/"' + \"\\n\" + '"}"
-      fi
-      echo "$escaped"
-    }
-
-    set_config() {
-      key="$1"
-      value="$2"
-      var_type="${3:-string}"
-      start="(['\"])$(sed_escape_lhs "$key")\2\s*,"
-      end="\);"
-      if [ "${key:0:1}" = '$' ]; then
-        start="^(\s*)$(sed_escape_lhs "$key")\s*="
-        end=";"
-      fi
-      sed -ri -e "s/($start\s*).*($end)$/\1$(sed_escape_rhs "$(php_escape "$value" "$var_type")")\3/" $MOUNTPOINT/shared/wp-config.php
-    }
-
-    set_config 'DB_HOST' "$WORDPRESS_DB_HOST"
-    set_config 'DB_USER' "$WORDPRESS_DB_USER"
-    set_config 'DB_PASSWORD' "$WORDPRESS_DB_PASSWORD"
-    set_config 'DB_NAME' "$WORDPRESS_DB_NAME"
-    set_config 'WP_HOME' "$WORDPRESS_URL"
-    set_config 'WP_SITEURL' "$WORDPRESS_URL/wordpress"
-    set_config 'WP_CONTENT_URL' "${WORDPRESS_URL}/wp-content"
-    set_config 'WP_PLUGIN_URL' "${WORDPRESS_URL}/wp-content/plugins"
-
-    for unique in "${uniqueEnvs[@]}"; do
-      uniqVar="WORDPRESS_$unique"
-      if [ -n "${!uniqVar}" ]; then
-        set_config "$unique" "${!uniqVar}"
-      else
-        # if not specified, let's generate a random value
-        currentVal="$(sed -rn -e "s/define\(\s*((['\"])$unique\2\s*,\s*)(['\"])(.*)\3\s*\);/\4/p" $MOUNTPOINT/shared/wp-config.php)"
-        if [ "$currentVal" = 'put your unique phrase here' ]; then
-          set_config "$unique" "$(head -c1m /dev/urandom | sha1sum | cut -d' ' -f1)"
-        fi
-      fi
-    done
-
-    if [ "$WORDPRESS_TABLE_PREFIX" ]; then
-      set_config '$table_prefix' "$WORDPRESS_TABLE_PREFIX"
-    fi
-
-    if [ "$WORDPRESS_DEBUG" ]; then
-      set_config 'WP_DEBUG' 1 boolean
-    fi
-
-    TERM=dumb php -- <<'EOPHP'
-<?php
-// database might not exist, so let's try creating it (just to be safe)
-
-$stderr = fopen('php://stderr', 'w');
-
-// https://codex.wordpress.org/Editing_wp-config.php#MySQL_Alternate_Port
-//   "hostname:port"
-// https://codex.wordpress.org/Editing_wp-config.php#MySQL_Sockets_or_Pipes
-//   "hostname:unix-socket-path"
-list($host, $socket) = explode(':', getenv('WORDPRESS_DB_HOST'), 2);
-$port = 0;
-if (is_numeric($socket)) {
-  $port = (int) $socket;
-  $socket = null;
-}
-$user = getenv('WORDPRESS_DB_USER');
-$pass = getenv('WORDPRESS_DB_PASSWORD');
-$dbName = getenv('WORDPRESS_DB_NAME');
-
-$maxTries = 10;
-do {
-  $mysql = new mysqli($host, $user, $pass, '', $port, $socket);
-  if ($mysql->connect_error) {
-    fwrite($stderr, "\n" . 'MySQL Connection Error: (' . $mysql->connect_errno . ') ' . $mysql->connect_error . "\n");
-    --$maxTries;
-    if ($maxTries <= 0) {
-      exit(1);
-    }
-    sleep(3);
-  }
-} while ($mysql->connect_error);
-
-if (!$mysql->query('CREATE DATABASE IF NOT EXISTS `' . $mysql->real_escape_string($dbName) . '`')) {
-  fwrite($stderr, "\n" . 'MySQL "CREATE DATABASE" Error: ' . $mysql->error . "\n");
-  $mysql->close();
-  exit(1);
-}
-
-$mysql->close();
-EOPHP
-fi
-
-# make sure our shared content locations are present
-if [ ! -d $MOUNTPOINT/shared ]; then
-    mkdir -p $MOUNTPOINT/shared
-    chown www-data:www-data $MOUNTPOINT/shared
-    chmod 755 $MOUNTPOINT/shared
-fi
-
-# init wp-content if it's not already there
-if [ ! -d $MOUNTPOINT/shared/wp-content ]; then
-    cp -Rp $MOUNTPOINT/release/wordpress/wp-content $MOUNTPOINT/shared/
-    chown -R www-data $MOUNTPOINT/shared/wp-content
-fi
-
-# set security
-if [ -e $MOUNTPOINT/shared/wp-config.php ]; then
-  chmod 640 $MOUNTPOINT/shared/wp-config.php
-  chown www-data $MOUNTPOINT/shared/wp-config.php
-fi
-
-# final cleanup
-rm -f $MOUNTPOINT/shared/sed* || true
-
-# now that we're definitely done writing configuration, let's clear out the relevant envrionment variables (so that stray "phpinfo()" calls don't leak secrets from our code)
-for e in "${envs[@]}"; do
-    unset "$e"
-done
-
 exec "$@"
